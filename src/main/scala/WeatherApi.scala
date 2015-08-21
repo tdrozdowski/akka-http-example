71d8eb23961a102b40485d76b00707876527a391
Terry Drozdowski <tdrozdowski@gmail.com>
2015-08-20 20:58:44 -0700

    removed dead imports

--- src/main/scala/WeatherApi.scala
+++ src/main/scala/WeatherApi.scala
@@ -3,18 +3,14 @@ import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.{ActorMaterializer, Materializer}
-
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
+import spray.json.DefaultJsonProtocol
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.ExecutionContextExecutor
-
-import spray.json.DefaultJsonProtocol
 
 /**
  * Created with IntelliJ IDEA.
